16423f616e5b4de5ffd9c570f1e1e34a2af19257
Avery <147835581+avery-codes@users.noreply.github.com>
2024-08-04 06:23:59 -0700

    Add files via upload
    During my interview I wrote String[] availableSpaces instead of Space[] availableSpaces. I finished the module quickly after I corrected this error.

--- Autoplayer.java
+++ Autoplayer.java
@@ -2,19 +2,14 @@ import java.util.Random;
 public class Autoplayer {
 
 
-    /*
-        send this class the list of available spaces each time
-        get number of remaining of available spaces
-        random number % # of available spaces
-        return a move
+    public String autoMove(Space[] availableSpaces){
+        int size;
+        int moveIndex;
+        Random random = new Random();
 
+        size = availableSpaces.length;
+        moveIndex = random.nextInt(size);
 
+        return availableSpaces[moveIndex].display;
     }
-    */
-
-
-
-
-
-
 }
